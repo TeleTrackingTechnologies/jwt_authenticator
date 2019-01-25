af969b91898ed41965e0293654899e5acb25b0c3
mikenac <mike-nacey@teletracking.com>
2019-01-25 15:54:04 -0500

    1.0

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='jwt_authenticator',
-    version='0.1',
+    version='1.0.0',
     author="Mike Nacey",
     author_email="nobody@teletracking.com",
     description="Simple JWT token flask service security library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/javatechy/dokr",
+    url="https://github.com/TeleTrackingTechnologies/jwt_authenticator",
     packages=['jwt_authenticator'],
     install_requires=['Flask', 'python-jose'],
     classifiers=[
